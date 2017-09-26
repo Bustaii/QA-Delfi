3c46043c8c97f59606d3456e938de2dad023daaa
bustaii <bustaone@gmail.com>
2017-09-27 00:33:52 +0300

    done in page comparison

--- src/test/java/Delfi/DimaStyle.java
+++ src/test/java/Delfi/DimaStyle.java
@@ -6,24 +6,31 @@ import org.testng.annotations.*; //selected all - TestNG
 import org.openqa.selenium.firefox.*; //selected all - required for firefox driver
 import java.util.*; //selected all - required for arrays
 
+import static java.lang.Integer.valueOf;
+
 public class DimaStyle {
     private String WEB_URL = "http://www.delfi.lv/"; //define desktop website url
     private String MOB_URL = "http://m.delfi.lv"; //define mobile website url
 
+    //WEB
     private static final By ARTICLE = By.xpath("//h3[@class='top2012-title']");
     private static final By ARTICLE_TITLE = By.xpath("//a[@class='top2012-title']");
     private static final By ARTICLE_COMMENT_COUNT = By.xpath("//a[@class='comment-count']");
+    private static final By ARTICLE_TITLE_PAGE = By.xpath("//h1[@class='article-title']/span");
+    private static final By ARTICLE_COMMENT_COUNT_PAGE = By.xpath("//a[@class='comment-count']");
 
+    //MOBILE
     private static final By ARTICLE_MOBILE = By.xpath("//div[@class='md-mosaic-title']");
     private static final By ARTICLE_TITLE_MOBILE = By.xpath("//a[@class='md-scrollpos']");
     private static final By ARTICLE_COMMENT_COUNT_MOBILE = By.xpath("//a[@class='commentCount']");
+    private static final By ARTICLE_TITLE_MOBILE_PAGE = By.xpath("//div[@class='article-title']/h1");
+    private static final By ARTICLE_COMMENT_COUNT_MOBILE_PAGE = By.xpath("//a[@class='commentCount']");
 
     @Test
     public void testMethod() {
         System.setProperty("webdriver.gecko.driver", "G:\\_driver/geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
-
 
         driver.get(WEB_URL);
         String newTab = Keys.chord(Keys.CONTROL, Keys.RETURN);
@@ -39,11 +46,15 @@ public class DimaStyle {
         driver.get(MOB_URL); //open this url in new tab\
         driver.switchTo().window(browserTabs.get(0)); // switch back to new tab [0] first tab
 
+        //WEB
         List<String> titlesArray = new ArrayList<String>();
         List<Integer> countArray = new ArrayList<Integer>();
+        List<String> urlArray = new ArrayList<String>();
 
+        //MOBILE
         List<String> titlesArrayMobile = new ArrayList<String>();
         List<Integer> countArrayMobile = new ArrayList<Integer>();
+        List<String> urlArrayMobile = new ArrayList<String>();
 
         List<WebElement> articleList = driver.findElements(ARTICLE);
         for (int i = 0; i < 5; i++) {
@@ -51,10 +62,13 @@ public class DimaStyle {
             String title = element.findElements(ARTICLE_TITLE).get(i).getText();
             titlesArray.add(title);
 
+            String url = element.findElements(ARTICLE_TITLE).get(i).getAttribute("href"); // get url
+            urlArray.add(url);
+
             if (element.findElements(ARTICLE_COMMENT_COUNT).size() != 0) {
                 String countToParse = element.findElements(ARTICLE_COMMENT_COUNT).get(i).getText(); //(106)
                 countToParse = countToParse.substring(countToParse.indexOf('(') + 1, countToParse.indexOf(')')); //106
-                countArray.add(Integer.valueOf(countToParse));
+                countArray.add(valueOf(countToParse));
             } else {
                 countArray.add(0);
             }
@@ -65,13 +79,16 @@ public class DimaStyle {
         List<WebElement> articleListMobile = driver.findElements(ARTICLE_MOBILE);
         for (int i = 0; i < 5; i++) {
             WebElement elementMobile = articleListMobile.get(i);
-            String title = elementMobile.findElements(ARTICLE_TITLE_MOBILE).get(i).getText();
-            titlesArrayMobile.add(title);
+            String titleMobile = elementMobile.findElements(ARTICLE_TITLE_MOBILE).get(i).getText();
+            titlesArrayMobile.add(titleMobile);
+
+            String urlMobile = elementMobile.findElements(ARTICLE_TITLE_MOBILE).get(i).getAttribute("href"); // get url
+            urlArrayMobile.add(urlMobile);
 
             if (elementMobile.findElements(ARTICLE_COMMENT_COUNT_MOBILE).size() != 0) {
-                String countToParse = elementMobile.findElements(ARTICLE_COMMENT_COUNT_MOBILE).get(i).getText(); //(106)
-                countToParse = countToParse.substring(countToParse.indexOf('(') + 1, countToParse.indexOf(')')); //106
-                countArrayMobile.add(Integer.valueOf(countToParse));
+                String countToParseMobile = elementMobile.findElements(ARTICLE_COMMENT_COUNT_MOBILE).get(i).getText(); //(106)
+                countToParseMobile = countToParseMobile.substring(countToParseMobile.indexOf('(') + 1, countToParseMobile.indexOf(')')); //106
+                countArrayMobile.add(valueOf(countToParseMobile));
             } else {
                 countArrayMobile.add(0);
             }
@@ -80,21 +97,47 @@ public class DimaStyle {
         //WEB
         System.out.println(titlesArray); // print array
         System.out.println(countArray); // print array
+        System.out.println(urlArray); // print array
         //MOBILE
         System.out.println(titlesArrayMobile); // print array
         System.out.println(countArrayMobile); // print array
+        System.out.println(urlArrayMobile); // print array
 
+        //Assert.assertEquals(titlesArray, titlesArrayMobile); // compare two arrays of titles
+        //Assert.assertEquals(countArray, countArrayMobile); // compare two arrays of comment count
 
-        Assert.assertEquals(titlesArray, titlesArrayMobile); // compare two arrays of titles
-        Assert.assertEquals(countArray, countArrayMobile); // compare two arrays of titles
-         // compare two arrays of titles
-//        if (Arrays.equals(countArray, countArrayMobile) ) {
-//        } else {
-//            System.out.println("GOOD"); // print array
-//        }
+        //MOBILE
+        for (int i = 0; i < 5; i++) {
+            String getArrayTitle = titlesArrayMobile.get(i);
+            String getArrayUrl = urlArrayMobile.get(i);
+            Integer getArrayCount = countArrayMobile.get(i);
+                driver.get(getArrayUrl);
+                    String findTitle = driver.findElement(ARTICLE_TITLE_MOBILE_PAGE).getText();
+                    Assert.assertTrue(findTitle.contains(getArrayTitle), "\n" + "Looking : " + getArrayTitle + "\n" + "Results : " + findTitle + "\n");
 
+                    String findCount = driver.findElement(ARTICLE_COMMENT_COUNT_MOBILE_PAGE).getText();
+                    findCount = findCount.substring(findCount.indexOf('(') + 1, findCount.indexOf(')')); //106
 
-//idsToCheck.add(Long.valueOf(element.findElement(ARTICLE_TITLE).getAttribute("href").substring()));
+                    //Assert.assertEquals(getArrayCount, findCount); // compare two arrays of comment count
+            System.out.println(findCount); // print array
+        }
+
+        //WEB
+        for (int i = 0; i < 5; i++) {
+            String getArrayTitle = titlesArray.get(i);
+            String getArrayUrl = urlArray.get(i);
+            Integer getArrayCount = countArray.get(i);
+                driver.get(getArrayUrl);
+                    String findTitle = driver.findElement(ARTICLE_TITLE_PAGE).getText();
+                    Assert.assertTrue(findTitle.contains(getArrayTitle), "\n" + "Looking : " + getArrayTitle + "\n" + "Results : " + findTitle + "\n");
+
+                    String findCount = driver.findElement(ARTICLE_COMMENT_COUNT_PAGE).getText();
+                    findCount = findCount.substring(findCount.indexOf('(') + 1, findCount.indexOf(')')); //106
+
+                    //Assert.assertEquals(getArrayCount, findCount); // compare two arrays of comment count
+            System.out.println(findCount); // print array
+        }
+
 
         //Kak perejti na tretju statju iz lista
 //        driver.get("http://delfi.lv/blablabla?id=" + idsToCheck.get(2));
